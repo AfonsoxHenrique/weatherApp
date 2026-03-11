248fb46a4539296a0ef898a9f14b7bda4045dbf0
Afonso Henrique <afonso1240avd@hotmail.com>
2026-03-11 23:20:02 +1100

    Login Screen Added
    Database to be connected later

---
+++ weatherApp/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  weatherApp
+//
+//  Created by Afonso Henrique Freitas de Paula on 11/3/2026.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
